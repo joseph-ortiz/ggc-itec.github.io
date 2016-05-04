325df1b5590416e338b44b5fed2347d93288aaf2
jortiz <josepho011235@gmail.com>
2016-05-03 22:22:46 -0400

    init react version

---
+++ app/App.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+
+ReactDOM.render(
+	<div>Hello World</div>	,
+	document.getElementById('app')
+);
